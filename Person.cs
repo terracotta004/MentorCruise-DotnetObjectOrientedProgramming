35c23d946cea02c9d8d798da246775086d269d5e
Ben Underwood <javerikr@gmail.com>
2025-11-09 21:11:15 -0500

    Classes and Objects task

---
+++ Person.cs
@@ -0,0 +1,16 @@
+﻿namespace DotnetMentorshipAssignment2;
+
+public class Person
+{
+    public string? Name;
+    public int Age;
+    public void SayHello()
+    {
+        Console.WriteLine($"Hello, my name is {Name}.");
+    }
+    public void CelebrateBirthday()
+    {
+        Age++;
+        Console.WriteLine($"Happy birthday! You are now {Age} years old.");
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿// See https://aka.ms/new-console-template for more information
+// Console.WriteLine("Hello, World!");
+
+var person1 = new DotnetMentorshipAssignment2.Person();
+
+person1.Name = "Alice";
+person1.Age = 30;
+
+person1.SayHello();
+person1.CelebrateBirthday();
+
+var person2 = new DotnetMentorshipAssignment2.Person();
+
+person2.Name = "Bob";
+person2.Age = 25;
+person2.SayHello();
+person2.CelebrateBirthday();
+
+
+
